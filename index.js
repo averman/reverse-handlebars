ca40a8be7071a3560dbf622b4b36d8341274c1ea
Arkan Gilang <ec2-user@ip-172-31-4-1.ap-southeast-1.compute.internal>
2018-12-14 09:06:19 +0000

    preq templating

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Helpers = require("./helpers");
 const resolve = require("./resolveBlock")
 const utils = require("./utils.js")
 const handlebars = require("handlebars");
+const flatten = require('flat');
 
 let handlePlus = { 
     reverse: function(template,content){
@@ -19,8 +20,32 @@ let handlePlus = {
     },
     resolve: resolve,
     helpers: Helpers,
-    apply: function(template,data){
-        return handlebars.compile(template)(data);
+    checkPreq: function(data,preq){
+        const prqs = flatten(preq);
+        const d = Object.assign({},data);
+        const res = {};
+        for(const p in prqs){
+            let posd = data;
+            for(const i of p.split('.')){
+                if(posd[i])posd=posd[i];
+                else{
+                    res[p]=prqs[p];
+                }
+            }
+        }
+        return res;
+    },
+    apply: function(template,data,preq){
+        let allowed = true;
+        if(preq){
+            let uns = this.checkPreq(data,preq);
+            uns = flatten.unflatten(uns);
+            if(JSON.stringify(uns)!='{}')
+                allowed=false;
+        }
+        if(allowed)
+            return handlebars.compile(template)(data);
+        throw new Error('prerequisite not statisfied');
     },
     registerHelper: function(helper){
         handlebars.registerHelper(helper.helperName, helper.handlebars)
